a417ef67e8df0150d7cc5b2d988abfa008e3ac87
MohammadMilad7 <milad.n21@yahoo.com>
2022-06-17 03:13:56 +0430

    complete the project and set the time and add zero before the number less than 10

---
+++ README.md
@@ -0,0 +1,3 @@
+this project help learned about how to make dark and light modes.
+I think the most important thing in our life is time, this project remembered me, time is gold!
+like always thanks :https://www.youtube.com/c/CodingLabYT
--- script.js
+++ script.js
@@ -4,3 +4,29 @@ let section = document.querySelector("section"),
  icons.onclick = () =>{
     section.classList.toggle("dark");
  }
+
+//  create a Function and calling it in every second 
+
+setInterval(() => {
+    let date = new Date(),
+    hour = date.getHours(),
+    min = date.getMinutes(),
+    sec = date.getSeconds();
+
+    let d;
+    d = hour < 12 ? "AM" : "PM"; 
+    hour = hour > 12 ? hour - 12 : hour;
+    hour = hour == 0 ? hour = 12 : hour;
+
+    // adding 0 to the all number thats less than 10
+
+    sec = sec < 10 ? "0" + sec : sec;
+    min = min < 10 ? "0" + min : min;
+    hour = hour < 10 ? "0" + hour : hour;
+
+
+    document.querySelector(".hour_num").innerText = hour;
+    document.querySelector(".min_num").innerText = min;
+    document.querySelector(".sec_num").innerText = sec;
+    document.querySelector(".am_pm").innerText = d;
+},1000); // 1000 milliseconds = 1s
